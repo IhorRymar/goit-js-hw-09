4eb3d0340048028bded79e27ccf34cda0c863e74
IhorRymar <95086464+IhorRymar@users.noreply.github.com>
2022-06-24 00:52:39 +0300

    i hope, the last changes

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,16 +2,13 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('.form'),
-  delay: document.querySelector('[name=delay]'),
-  step: document.querySelector('[name=step]'),
-  amount: document.querySelector('[name=amount]'),
 };
 
-let position = 0;
+refs.form.addEventListener('submit', submitForm);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -20,25 +17,22 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
-  return promise;
 }
-
-refs.form.addEventListener('submit', submitForm);
 
 function submitForm(e) {
   e.preventDefault();
 
-  setTimeout(() => {
-    for (let i = 1; i < refs.amount.value; i += 1) {
-      let totalStep = Number(refs.delay.value) + Number(refs.step.value) * position;
-      position = i + 1;
-      createPromise(i, totalStep)
-        .then(({ position, delay }) => {
-          Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-        });
-    }
-  }, refs.delay.value);
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+  createProm(+delay.value, +step.value, amount.value);
+}
+
+function createProm(delay, step, amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => Notify.success(`Fulfilled promise ${position} in ${delay}ms`))
+      .catch(({ position, delay }) => Notify.failure(`Rejected promise ${position} in ${delay}ms`));
+    delay += step;
+  }
 }
